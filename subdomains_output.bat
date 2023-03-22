684d635997b3f2708b401553193e1cca93ace869
tiahelou10 <tia.elhelou@outlook.com>
2023-03-22 17:59:25 +0200

    run of url checker

---
+++ subdomains_output.bat
@@ -0,0 +1,9 @@
+mailudemy.com: is reachable 
+mail2udemy.com: is reachable 
+wwwudemy.com: is reachable 
+ns2udemy.com: is reachable 
+ns1udemy.com: is reachable 
+blogudemy.com: is Not reachable, status_code: 403 
+localhostudemy.com: is reachable 
+mudemy.com: is reachable 
+ftpudemy.com: is reachable 
